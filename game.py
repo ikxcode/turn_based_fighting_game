8d44831b9224f77f4c75d9fdd54034de908835df
ikx <ihsankx@gmail.com>
2019-10-29 21:32:10 +0000

    added block

--- game.py
+++ game.py
@@ -7,9 +7,10 @@ class Game:
         play = "y"
         total_wins = 0
         round = 0
+        block = 0
         while play == "y":
             round = round + 1
-            new_round = GameRound(round)
+            new_round = GameRound(round, block)
             won = new_round.start()
             play = input("Do you want to play again? (y/n) ")
             play = play.lower()
@@ -25,8 +26,9 @@ class GameRound:
     player_hp = 10
     enemy_hp = 10
 
-    def __init__(self, game_round):
+    def __init__(self, game_round, block):
         self.game_round = game_round
+        self.block = block
 
     def start(self):
         print("Round " + str(self.game_round) + ". FIGHT!")
@@ -47,8 +49,8 @@ class GameRound:
     def play_turn(self):
         action = ""
         print("player turn")
-        while action != "attack" and action != "heal":
-            action = input("Do you want to 'attack' or 'heal'? ").lower().strip()
+        while action != "attack" and action != "heal" and action != "block":
+            action = input("Do you want to 'attack', 'heal' or 'block' ").lower().strip()
             if action == "attack":
                 damage = random.randint(0, 5)
                 self.enemy_hp = self.enemy_hp - damage
@@ -56,8 +58,20 @@ class GameRound:
             elif action == "heal":
                 heal = random.randint(0, 5)
                 self.player_hp = self.player_hp + heal
+                print("You healed " + str(heal) + " health! Your current health is " + str(self.player_hp))
+            elif action == "block":
+                self.block = random.randint(0, 5)
+                print("You will block " + str(self.block) + " damage next round ")
             else:
                 print("Please enter a valid answer")
 
     def enemy_turn(self):
-        print("enemy turn")
+        if self.enemy_hp > 0:
+            print("enemy turn")
+            enemy_damage = random.randint(0, 5)
+            enemy_damage = enemy_damage - self.block
+            if enemy_damage > 0:
+                self.player_hp = self.player_hp - enemy_damage
+                print("The enemy dealt " + str(enemy_damage) + " damage! Your remaining health is " + str(self.player_hp))
+            else:
+                print("The enemy dealt no damage!")
